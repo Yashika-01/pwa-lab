d8509eea2592f93979c1d0f2f82a6ae9e54c0029
huzaifa anjum <huzaifafrit@outlook.com>
2022-03-23 05:42:52 +0530

    Initialize project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
+import { AuthModule } from './auth/auth.module';
+import { CartModule } from './cart/cart.module';
+import { MaterialModule } from './material.module';
+import { ProductModule } from './product/product.module';
+import { TitleCasePipe } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,9 +19,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    MaterialModule,
+    FormsModule,
+    ProductModule,
+    AuthModule,
+    CartModule
   ],
-  providers: [],
+  providers: [TitleCasePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
